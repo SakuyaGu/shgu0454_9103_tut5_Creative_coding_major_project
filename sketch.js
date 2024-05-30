99a98b7ca2e0fd1d8355856830edab1b1434d71e
Shengyuan Gu <shgu0454@uni.sydney.edu.au>
2024-05-31 02:40:54 +1000

    Updating the color logic
    Introduces new variables colorInt and colorCount. colorInt is colorCount divided by 255, so colorInt ranges from 0 to 1. This means that when colorCount varies from 0 to 255, colorInt corresponds to the range 0 to 1. This variable is used to make the color change softly and not drastically by stepping from 0 to 1 and then from 1 to 0 in a period of 10ms.
    Also replace the random() function used in all drawing of graphics to solve the problem of color change flashing when playing music.
    I tried to randomly generate pink curves at the circle's center, but the actual effect was not visually appealing, so I commented out the corresponding code. Next time it can be modified if a better presentation comes to mind.

--- sketch.js
+++ sketch.js
@@ -13,12 +13,9 @@ let fft;
 let numBins = 128;
 let smoothing = 0.8;
 
+
 function minWindowSize(){
   return min(windowWidth,windowHeight);
-}
-
-function randomColor(){
-  return color(random(255),random(255),random(255));
 }
 
 function preload() {
@@ -34,11 +31,12 @@ function setup() {
   fft = new p5.FFT(smoothing, numBins);
   song.connect(fft);
 
+  colorMode(HSB, 255);
+
   background(0, 84, 121);
   
   noLoop();
 }
-
 
 
 function circleRing(centerX, centerY, freqEnergy){
@@ -49,7 +47,7 @@ function circleRing(centerX, centerY, freqEnergy){
   let cornerRadius = 8;
   let layerNum = freqEnergy / 30;
   let s = 5/layerNum;
-  fill(freqEnergy * random() * 3, freqEnergy * random() * 5, freqEnergy * random() * 3);
+  fill(freqEnergy * colorInt, freqEnergy, 255);
   stroke(0,0);
   for(let a = 0; a < layerNum; a++){
 
@@ -71,6 +69,11 @@ function circleRing(centerX, centerY, freqEnergy){
   
 }
 
+function mouseMoved() {
+  // map the mouseY to a volume value between 0 and 1
+  volume = map(mouseY, 0, height, 1, 0);
+  song.setVolume(volume);
+}
 
 function mousePressed() {
   if (song.isPlaying()) {
@@ -85,19 +88,19 @@ function mousePressed() {
 
 function drawConcentricCircles(centerX, centerY, maxDiameter, numCircles, freqEnergy) {
   let step = maxDiameter / numCircles;
-  let e = freqEnergy * random(1, 255);
+  let e = freqEnergy * colorInt;
   for (let i = 0; i < numCircles; i++) {
     let diameter = maxDiameter - i * step;
     let offsetX = random(-1, 1);
     let offsetY = random(-1, 1);
     if(i<6){
-      fill(e, e, e);
+      fill(e, e * 2, e * 3);
     }
      else{
-      fill(freqEnergy * random() * 4, freqEnergy * random() * 5, freqEnergy * random() * 3);
+      fill(e / 2 , e * 2, e * 3);
      } 
-     stroke(freqEnergy * random() * 10, freqEnergy * random() * 5, freqEnergy * random() * 3);
-     strokeWeight(freqEnergy * random(0, 5) / 50);
+     stroke(freqEnergy * colorInt * 0.3, freqEnergy, freqEnergy);
+     strokeWeight(1);
     ellipse(centerX + offsetX, centerY + offsetY, diameter, diameter);
   }
 }
@@ -105,7 +108,7 @@ function drawConcentricCircles(centerX, centerY, maxDiameter, numCircles, freqEn
 //draw line and dots for circle1 & 2
 function drawCircleDots(centerX, centerY, radius, numDots, dot, freqEnergy) {
   let angleStep = TWO_PI / numDots;
-  fill(freqEnergy * random() * 10, freqEnergy * random() * 5, freqEnergy * random() * 3);
+  fill(freqEnergy * colorInt * random(), 255, 255);
   noStroke();
   for (let i = 0; i < numDots; i++) {
     let angle = i * angleStep;
@@ -119,7 +122,7 @@ function drawCircleLines(centerX, centerY, startRadius, numLines, lineLength, fr
   strokeWeight(1);
   for (let i = 0; i < numLines; i++) {
     let angle = TWO_PI / numLines * i;
-    stroke(freqEnergy * random() * 10, freqEnergy * random() * 5, freqEnergy * random() * 3);
+    stroke(freqEnergy * colorInt * 10, freqEnergy * colorInt * 5, freqEnergy * colorInt * 3);
     let xStart = centerX + cos(angle) * startRadius; 
     let yStart = centerY + sin(angle) * startRadius; 
     let xEnd = centerX + cos(angle) * (startRadius + lineLength); 
@@ -136,12 +139,12 @@ function Circle1(centerX, centerY, freqEnergy){
   let radiusIncrement = 5;
   let numLayers = 4; 
  //circle out side
-  fill(255, 204, 0);
+  fill(freqEnergy * colorInt * 0.25, 204, 255);
   noStroke();
   circle(centerX, centerY, map(freqEnergy, 0, 255, 100, 140));
 
   // circle inside
-  fill(freqEnergy * random() * 10, freqEnergy * random() * 5, freqEnergy * random() * 3);
+  fill(freqEnergy * colorInt  , 255 / colorInt, 255 / colorInt);
   noStroke();
   circle(centerX, centerY, map(freqEnergy, 0, 255, 100, 140) / 2);
 
@@ -159,10 +162,10 @@ function Circle2(centerX, centerY, freqEnergy) {
   let radiusStep = 4;
   let initialNumDots = 40;
   let dotsIncrement = 6;
-  fill(freqEnergy * random() * 3, freqEnergy * random() * 5, freqEnergy * random() * 3);
+  fill(freqEnergy * colorInt * 3, 255, 255);
   noStroke();
   circle(centerX, centerY, map(freqEnergy, 0, 255, 100, 140) / 2);
-  fill(freqEnergy * random() * 2, freqEnergy * random() * 4, freqEnergy * random() * 3);
+  fill(freqEnergy * colorInt * 2, 255, 255);
   ellipse(centerX, centerY, initialRadius, initialRadius);
   for (let i = 0; i < numLayers; i++) {
     let radius = initialRadius + i * radiusStep;  
@@ -190,7 +193,7 @@ function Circle3(centerX, centerY, freqEnergy){
     }
   }
   strokeWeight(1);
-  stroke(freqEnergy * random() * 2, freqEnergy * random() * 2, freqEnergy * random() * 3);
+  stroke(freqEnergy * colorInt * 2, freqEnergy * colorInt * 2, freqEnergy * colorInt * 3);
   noFill();
   beginShape();
   for (let p of points) {
@@ -218,37 +221,59 @@ function drawChain(centerX, centerY, chainRadius, numLinks) {
     let angle = i * angleStep;
     let x = centerX + cos(angle) * chainRadius;
     let y = centerY + sin(angle) * chainRadius;
-    fill(255);
+    fill(255, 0, 255);
     stroke(0);
     strokeWeight(2.5);
     ellipse(x+1, y+1, 6, 6); 
   }
 }
 
+let colorInt = 0;
+let colorCount = 0;
+let flag=true;
+window.setInterval(function() {  
+  if(flag){
+    colorCount++;
+    colorInt=colorCount/255;    
+  } else {
+    colorCount--;
+    colorInt=colorCount/255;
+  }
+  if(colorCount>=255){
+    flag=false;
+  } else if(colorCount<=0){
+    flag = true
+  }
+    
+},10)
+
+
+
 function draw() {
-  background(0, 84, 121);
+  background(142.375, 215,  150);
   let Size = minWindowSize();
   scale(Size/500);
-  let spectrum = fft.analyze();
+  let spectrum = fft.analyze();  
+  
   for(let i = 0; i < centerXs.length; i++){
     let x = centerXs[i];
     let y = centerYs[i];
-    let freqEnergy = spectrum[i % spectrum.length];
-    
-    fill(color(freqEnergy * random(180, 255) / 200, freqEnergy * random(180, 255) / 200, freqEnergy * random(180, 255) / 200));
-    stroke(randomColor() * freqEnergy);
+    let freqEnergy = spectrum[i % spectrum.length];      
+     
+    fill(freqEnergy , freqEnergy, freqEnergy);
+    stroke(freqEnergy, 255, 255); 
 
-    strokeWeight(freqEnergy * random() / 8);
+    strokeWeight(freqEnergy * colorInt / 20);
     circle(x, y, map(freqEnergy, 0, 255, 100, 140));
   
     drawChain(x, y, map(freqEnergy, 0, 255, 50, 80), 20); 
     drawConcentricCircles(x, y, map(freqEnergy, 0, 255, 40, 60), 8, freqEnergy);
     circleRing(x, y, freqEnergy);
      
-    if (freqEnergy) {
-      drawPinkCurve(x, y, 75);
+    // if (freqEnergy) {
+    //   drawPinkCurve(x, y, 75);
 
-    }
+    // }
   }
 
   for(let i = 0; i < circle1Xs.length; i++){
@@ -258,9 +283,9 @@ function draw() {
     Circle1(x, y, freqEnergy);
     drawChain(x, y, map(freqEnergy, 0, 255, 50, 80), 15);
 
-    if (freqEnergy) {
-      drawPinkCurve(x, y, map(freqEnergy, 0, 255, 50, 80));
-    }
+    // if (freqEnergy) {
+    //   drawPinkCurve(x, y, map(freqEnergy, 0, 255, 50, 80));
+    // }
   }
 
   for(let i = 0; i < circle2Xs.length; i++){
@@ -270,25 +295,23 @@ function draw() {
     Circle2(x, y, freqEnergy);
     drawChain(x, y, map(freqEnergy, 0, 255, 50, 80), 20);
 
-    if (freqEnergy) {
-      drawPinkCurve(x, y, 75);
-    }
+    // if (freqEnergy) {
+    //   drawPinkCurve(x, y, 75);
+    // }
   }
 
   for (let i = 0; i < circle3Xs.length; i++) {
     let freqEnergy = spectrum[i % spectrum.length];
     let x = circle3Xs[i];
     let y = circle3Ys[i];
-    fill(freqEnergy * random() * 4, freqEnergy * random() * 5, freqEnergy * random() * 3);
+    fill(freqEnergy * colorInt * 0.25, 255, 255);
     circle(x, y, map(freqEnergy, 0, 255, 50, 80) * 2);
     Circle3(x, y, freqEnergy);
-    fill(freqEnergy * random() * 4, freqEnergy * random() * 5, freqEnergy * random() * 3);
-    circle(x, y, map(freqEnergy, 0, 255, 50, 80));
     drawConcentricCircles(x, y, map(freqEnergy, 0, 255, 50, 80), 5, freqEnergy);
 
-    if (freqEnergy) {
-      drawPinkCurve(x, y, map(freqEnergy, 0, 255, 50, 80));
-    }
+    // if (freqEnergy) {
+    //   drawPinkCurve(x, y, map(freqEnergy, 0, 255, 50, 80));
+    // }
   }
 }
 
